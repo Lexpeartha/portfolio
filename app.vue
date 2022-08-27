c42b5f752b368e1a419c46a62f5bee6a8f97f505
Aleksa <sevic.official@gmail.com>
2022-08-27 22:13:13 +0200

    style: add background pattern

--- app.vue
+++ app.vue
@@ -21,24 +21,4 @@ useHead({
 body {
   @apply transition-colors duration-150 bg-primary-50 dark:bg-primary-900;
 }
-
-/* Overlay texture */
-// body::after {
-//   content: '';
-//   opacity: 8%;
-//   display: block;
-//   height: 100vh;
-//   width: 100vw;
-//   position: fixed;
-//   pointer-events: none;
-//   top: 0;
-//   left: 0;
-//   z-index: 1000;
-//   background-image: url('@/assets/pattern.png');
-//   background-repeat: repeat;
-// }
-
-.light body::after {
-  opacity: 11%;
-}
 </style>
