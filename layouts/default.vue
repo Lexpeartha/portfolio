caa76c92078c4d13bf9b8d1dcffb8889580fdd9c
Aleksa <sevic.official@gmail.com>
2022-08-31 22:46:52 +0200

    style: change to appropriate prose class

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="z-0 max-w-2xl px-4 py-10 m-auto bg-white sm:px-8 sm:shadow dark:bg-primary-800 sm:rounded">
-    <main class="prose prose-stone dark:prose-invert max-w-none">
+    <main class="prose prose-primary dark:prose-invert max-w-none">
       <slot />
     </main>
   </div>
