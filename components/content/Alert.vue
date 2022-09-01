2d2b599b7b3d1036ad7aa3edd0406f96ea780c74
Aleksa <sevic.official@gmail.com>
2022-09-01 20:32:11 +0200

    style: update alert component

--- components/content/Alert.vue
+++ components/content/Alert.vue
@@ -5,19 +5,20 @@ defineProps({
     default: 'ion:warning'
   }
 })
+// text-secondary-600 dark:text-secondary-200 bg-secondary-100 dark:bg-secondary-900
 </script>
 
 <template>
-  <div class="p-4 rounded-md dark:bg-amber-100 bg-amber-50">
+  <div class="p-4 rounded-lg dark:bg-yellow-900 bg-yellow-50">
     <div class="flex">
       <div v-if="icon" class="flex-shrink-0 mr-3">
-        <Icon class="w-5 h-5 text-yellow-600" aria-hidden="true" :name="icon" />
+        <Icon class="w-5 h-5 text-yellow-900 dark:text-yellow-200" aria-hidden="true" :name="icon" />
       </div>
       <div>
-        <h3 class="m-0 text-base font-medium text-yellow-800">
+        <h3 class="m-0 text-base font-medium text-yellow-900 dark:text-yellow-200">
           <ContentSlot :use="$slots.title" unwrap="p" />
         </h3>
-        <div class="mt-2 text-sm text-yellow-700">
+        <div class="mt-2 text-sm text-yellow-700 dark:text-yellow-100">
           <ContentSlot :use="$slots.default" unwrap="p" />
         </div>
       </div>
