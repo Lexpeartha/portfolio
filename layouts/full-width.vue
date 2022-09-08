67f747a8366dc35ede51daec43e304f25b8855cc
Aleksa <sevic.official@gmail.com>
2022-09-08 20:42:47 +0200

    style: update full-width layout

--- layouts/full-width.vue
+++ layouts/full-width.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="z-0 py-10 m-auto bg-white sm:px-8 sm:shadow dark:bg-primary-800 sm:rounded">
-    <main class="max-w-2xl px-4 mx-auto prose sm:px-8 prose-primary dark:prose-invert">
+    <main class="max-w-2xl xl:max-w-4xl px-4 mx-auto prose sm:px-8 prose-primary dark:prose-invert">
       <slot>
         <h1 class="text-2xl">
           Page not found
