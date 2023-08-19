2c6f4912c23f4a8177da5f945ce89103dc32697d
Lexpeartha <sevic.official@gmail.com>
2023-08-19 17:34:25 +0200

    chore: add header content type header to `sitemap.xml`

--- server/routes/sitemap.xml.ts
+++ server/routes/sitemap.xml.ts
@@ -4,10 +4,14 @@ import { serverQueryContent } from '#content/server'
 export default defineEventHandler(async (event) => {
   // Fetch all documents
   const docs = await serverQueryContent(event).find()
+
+  // Add headers
+  event.node.res.setHeader('Content-Type', 'application/xml')
+
+  // Create sitemap stream and add items to it
   const sitemap = new SitemapStream({
     hostname: 'https://www.lexpeartha.com'
   })
-
   for (const doc of docs.filter((d) => {
     const items = d._path?.split('/')
     return !(items?.at(-1) as string).startsWith('_')
@@ -24,5 +28,6 @@ export default defineEventHandler(async (event) => {
   }
   sitemap.end()
 
+  // Returns stream as promise
   return streamToPromise(sitemap)
 })
