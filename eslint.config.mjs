5bccd741d3886dbac5897be300723914329fb526
Lexpeartha <sevic.official@gmail.com>
2026-02-18 21:35:45 +0100

    chore: update deps & retouching of index page

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,12 +1,16 @@
 // @ts-check
 import withNuxt from './.nuxt/eslint.config.mjs'
 
-export default withNuxt().override('nuxt/vue/rules', {
-  rules: {
-    'vue/html-self-closing': 'off',
-  },
-}).override('nuxt/typescript/rules', {
-  rules: {
-    '@typescript-eslint/no-explicit-any': 'warn',
-  },
+export default withNuxt({
+  ignores: ['dist', 'node_modules', '.output', '.nuxt'],
 })
+  .override('nuxt/vue/rules', {
+    rules: {
+      'vue/html-self-closing': 'off',
+    },
+  })
+  .override('nuxt/typescript/rules', {
+    rules: {
+      '@typescript-eslint/no-explicit-any': 'warn',
+    },
+  })
